734fc552670d52561edd2502701917d184bb6699
Heshan Tharindu Kalubowila <55088720+heshant3@users.noreply.github.com>
2022-10-25 21:10:24 +0530

    Update console.js

--- console.js
+++ console.js
@@ -5,6 +5,16 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
+}
+
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+
+
 }// Your First Program
 
 
